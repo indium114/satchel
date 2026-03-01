e9631aa2d0c51d2c0e5cbb3fb351d9cea05592d2
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-01 18:49:58 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "satchel devshell and package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "satchel-devshell";
+
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            delve
+          ];
+        };
+
+        packages.satchel = pkgs.buildGoModule {
+          pname = "satchel";
+          version = "2026.03.01-a";
+
+          src = self;
+
+          vendorHash = pkgs.lib.fakeHash;
+
+          subPackages = [ "." ];
+          ldflags = [ "-s" "-w" ];
+
+          meta = with pkgs.lib; {
+            description = "A simple command-line file clipboard to make file management easier";
+            license = licenses.mit;
+            platforms = platforms.all;
+          };
+        };
+
+        apps.satchel = {
+          type = "app";
+          program = "${self.packages.${system}.satchel}/bin/satchel";
+        };
+      });
+}
